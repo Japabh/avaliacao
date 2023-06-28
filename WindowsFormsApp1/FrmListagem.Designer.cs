a670e99ee8d1f5f38b82ce884420d79596fb8099
Japabh <125514857+Japabh@users.noreply.github.com>
2023-06-27 21:58:46 -0300

    Add files via upload

---
+++ WindowsFormsApp1/FrmListagem.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FrmListagem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(643, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 79);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "FECHAR";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // FrmListagem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "FrmListagem";
+            this.Text = "FrmListagem";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
